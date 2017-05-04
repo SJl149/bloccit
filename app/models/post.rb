11ce8b7f11ca3ad992b5b620133c562b52cafb80
Scott Lenander <sjlteacher@gmail.com>
2017-05-04 09:46:32 -0400

    Assignment 31: Favorites is completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,6 +12,13 @@ class Post < ActiveRecord::Base
   validates :topic, presence: true
   validates :user, presence: true
 
+  after_create :favorite_post
+
+  def favorite_post
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
+  end
+
   def up_votes
     votes.where(value: 1).count
   end
